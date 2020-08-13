11f6d63862b126ebabc420761f7569adfdaaca2d
Joaquin Lousa <joaquin.lousa@gmail.com>
2020-08-14 01:35:27 +0200

    Tutorial python django 4.2

--- polls/views.py
+++ polls/views.py
@@ -2,16 +2,35 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the polls index.")
 
-def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	context = {'latest_question_list': latest_question_list}
-	return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+#def index(request):
+#	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#	context = {'latest_question_list': latest_question_list}
+#	return render(request, 'polls/index.html', context)
 
 
 	#template = loader.get_template('polls/index.html')
@@ -22,9 +41,9 @@ def index(request):
 	#output = ', '.join([q.question_text for q in latest_question_list])
 	#return HttpResponse(output)
 	
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+#def detail(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/detail.html', {'question': question})
 
     #try:
     #	question = Question.objects.get(pk=question_id)
@@ -37,9 +56,9 @@ def detail(request, question_id):
 #def results(request, question_id):
 #    response = "You're looking at the results of question %s."
 #    return HttpResponse(response % question_id)
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+#def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/results.html', {'question': question})
 
 #def vote(request, question_id):
 #    return HttpResponse("You're voting on question %s." % question_id)
